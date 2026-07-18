c23509257405ef0321132fc9803181f608e421bd
Nafis5858 <nafis5858@example.com>
2026-07-19 03:59:07 +0600

    Add config.php file

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// Database configuration for InfinityFree live server
+
+$dbHost = 'sql211.infinityfree.com';
+$dbName = 'if0_42442690_cse391_assignment03';
+$dbUser = 'if0_42442690';
+$dbPass = 'Nkz585883';
