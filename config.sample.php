4b2939f6f74e41673a768531379d96a3f156fe03
Nafis5858 <nafis5858@example.com>
2026-07-19 03:20:14 +0600

    Add config sample and update README for deployment readiness

---
+++ config.sample.php
@@ -0,0 +1,7 @@
+<?php
+// Copy this file to config.php and update the values for your local or remote environment.
+
+$dbHost = 'localhost';
+$dbName = 'car_workshop';
+$dbUser = 'root';
+$dbPass = '';
